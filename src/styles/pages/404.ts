e222f45af9467c853dd0d9ac414e9c86bcf4d06a
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-04-28 22:57:36 -0300

    Fixed 404 to develop

--- src/styles/pages/404.ts
+++ src/styles/pages/404.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 100vh;
+  min-height: calc(100vh - 5rem);
 
   display: flex;
   align-items: center;
