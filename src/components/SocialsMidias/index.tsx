de6178a15e153850268d771406f8eca5f8ac5133
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-08-03 14:21:02 -0300

    :bug: (fiz animation on index Page)

--- src/components/SocialsMidias/index.tsx
+++ src/components/SocialsMidias/index.tsx
@@ -1,34 +1,47 @@
-import { Parallax } from 'react-scroll-parallax';
-import { FaWhatsappSquare, FaLinkedin, FaFacebookSquare, FaGithub } from 'react-icons/fa'
+import { Parallax } from "react-scroll-parallax";
+import {
+  FaWhatsappSquare,
+  FaLinkedin,
+  FaFacebookSquare,
+  FaGithub,
+} from "react-icons/fa";
 
-import * as S from './styles'
+import * as S from "./styles";
 
 export function SocialsMidia() {
   return (
     <S.Container>
       <a
         href="https://github.com/jhonatanllima"
-        target="blank" className="animate__animated  animate__fadeIn animate__delay-2s">
+        target="blank"
+        className="animate__animated  animate__fadeIn"
+      >
         <FaGithub />
       </a>
 
       <a
         href="https://www.linkedin.com/in/jhonatan-lima-75a2a0142/"
-        target="blank" className="animate__animated  animate__fadeIn animate__delay-3s">
+        target="blank"
+        className="animate__animated  animate__fadeIn animate__delay-1s"
+      >
         <FaLinkedin />
       </a>
 
       <a
-        href="https://wa.me/5515996759630?text=Hey Jhonatan! Vim através do seu portifólio!"
-        target="blank" className="animate__animated  animate__fadeIn animate__delay-4s">
+        href="https://wa.me/5515996759630?text=Hey Jhonatan! Vim através do seu portfólio!"
+        target="blank"
+        className="animate__animated  animate__fadeIn animate__delay-2s"
+      >
         <FaWhatsappSquare />
       </a>
 
       <a
         href="https://www.facebook.com/jhonyllima/"
-        target="blank" className="animate__animated  animate__fadeIn animate__delay-5s">
+        target="blank"
+        className="animate__animated  animate__fadeIn animate__delay-3s"
+      >
         <FaFacebookSquare />
       </a>
     </S.Container>
-  )
+  );
 }
